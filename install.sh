071e12fb34494da514f95224f9d160cec07a52f4
BigOlWilly <120047071+BigOlWilly@users.noreply.github.com>
2023-02-22 20:00:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 sudo apt-get update
-sudo apt install python3-pip
+sudo apt install python-pip3
 
-sudo chmod +x start
+git clone https://github.com/MatrixTM/MHDDoS
+
+cd MHDDoS
+
+sudo pip3 install -r requirements.txt
+
+cd ..
 
 sudo python3 main.py
